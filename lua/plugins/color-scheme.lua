5fd5032e57763e2191f3379a625ff15ac1b63c6f
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-09-19 19:31:06 +0700

    using catppuccin

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -2,13 +2,22 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = true,
       },
     },
-    enabled = true,
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
